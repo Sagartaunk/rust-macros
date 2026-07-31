639bdb4be4306a4409fae0172783e8dd1749f889
Sagar Taunk <sagartaunk@proton.me>
2026-07-31 15:51:54 +0530

    Stop tracking target directories

--- proc-macross/target/debug/deps/proc_macross-4dc5428c1d809d73.d
+++
@@ -1,5 +0,0 @@
-/home/sagar/Documents/rust-macros/proc-macross/target/debug/deps/proc_macross-4dc5428c1d809d73.d: src/lib.rs
-
-/home/sagar/Documents/rust-macros/proc-macross/target/debug/deps/libproc_macross-4dc5428c1d809d73.rmeta: src/lib.rs
-
-src/lib.rs:
--- proc-macross/target/debug/deps/proc_macross-68d7b5d69671a819.d
+++
@@ -1,5 +0,0 @@
-/home/sagar/Documents/rust-macros/proc-macross/target/debug/deps/proc_macross-68d7b5d69671a819.d: src/lib.rs
-
-/home/sagar/Documents/rust-macros/proc-macross/target/debug/deps/libproc_macross-68d7b5d69671a819.rmeta: src/lib.rs
-
-src/lib.rs:
